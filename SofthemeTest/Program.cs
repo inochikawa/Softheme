63d2a1b21bc8d697f143cffe49c0f89e8bdfee37
msCube <max.stecenko.max@gmail.com>
2015-12-05 00:19:28 +0200

    Firs test

---
+++ SofthemeTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SofthemeTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test1 t1 = new Test1();
+
+            t1.CountNumbres();
+            t1.WriteToFile();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SofthemeTest/Test1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace SofthemeTest
+{
+    public class Test1
+    {
+        private string f_fileContent;
+        private StringBuilder f_result;
+
+        public Test1()
+        {
+            try
+            {
+                using (StreamReader streamReader = new StreamReader("INPUT.txt"))
+                    f_fileContent = streamReader.ReadToEnd();
+            }
+            catch (FileNotFoundException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+
+            f_result = new StringBuilder();
+        }
+
+        public void CountNumbres()
+        {
+            if (f_fileContent == null)
+            {
+                Console.WriteLine("File INPUT.txt was not read!");
+                return;
+            }
+            if (f_fileContent.Length > 100)
+            {
+                Console.WriteLine("Count of numbers is more than 100!");
+                return;
+            }
+
+            int counter = 0;
+
+            foreach (char word in f_fileContent)
+            {
+                if (int.Parse(word.ToString()) == 1)
+                {
+                    f_result.Append(1);
+                    counter++;
+                }
+            }
+
+            Console.WriteLine("Count of 1: {0}", counter);
+        }
+
+        public void WriteToFile()
+        {
+            try
+            {
+                using (StreamWriter streamWriter = new StreamWriter("OUTPUT.txt"))
+                {
+                    streamWriter.Write(f_result);
+                }
+            }
+            catch (FileNotFoundException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+
+
+            Console.WriteLine("Information was written to file OUTPUT.txt");
+        }
+    }
+}
